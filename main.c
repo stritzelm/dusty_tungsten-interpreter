5206717a86b190e105d54f28fb1f49f6df8506ce
lanierd <lanierd@carleton.edu>
2019-10-29 20:49:38 -0500

    starting stuff

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "tokenizer.h"
+#include "value.h"
+#include "linkedlist.h"
+#include "parser.h"
+#include "talloc.h"
+
+/* Main for parser, by Dave Musicant
+   Fairly self-explanatory. Tokenizes, parses, and prints out a parsed Racket program. */
+
+int main() {
+
+    Value *list = tokenize();
+    Value *tree = parse(list);
+
+    printTree(tree);
+    printf("\n");
+
+    tfree();
+    return 0;
+}
